9ef5cfd1f58829525abfa62ccc16b637725dafe5
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-05-19 10:36:29 -0400

    added character back

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (){
+let greenCharacter = document.createElement('img')    
+}
+
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
 
 let pineTree = document.createElement('img')
 pineTree.src = 'assets/pine-tree.png'
@@ -11,3 +15,5 @@ pineTree.style.position = 'fixed'
 pineTree.style.left = '450px'
 pineTree.style.bottom = '200px'
 document.body.append(pineTree)
+
+
