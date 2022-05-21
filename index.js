7b01180e865cf79cfe07ed3a4d17babf67438b04
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-05-21 01:18:21 -0400

    made edits

--- index.js
+++ index.js
@@ -4,11 +4,6 @@
 //     let greenCharacter = document.createElement('img')
 //     console.log('img')
 // ]
-
-=======
-function newImage(){
-    console.log('img')
-}
 >>>>>>> a7851462832fa444bff92fa2309145bb87050bcb
 
 let greenCharacter = document.createElement('img')
@@ -16,20 +11,7 @@ greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+greenCharacter.src = 'assets/green-character.gif'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a7851462832fa444bff92fa2309145bb87050bcb
 let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
 document.body.append(pineTree)
-<<<<<<< HEAD
-=======
-
-
->>>>>>> a7851462832fa444bff92fa2309145bb87050bcb
