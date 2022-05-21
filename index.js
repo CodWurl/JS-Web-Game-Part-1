f634abd4ed34736295e1e380d90e02e903219c86
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-05-21 01:19:41 -0400

    added green man and tree

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-<<<<<<< HEAD
+
 
 // funtion newImage()[
 //     let greenCharacter = document.createElement('img')
 //     console.log('img')
 // ]
->>>>>>> a7851462832fa444bff92fa2309145bb87050bcb
+
 
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
-greenCharacter.src = 'assets/green-character.gif'
+document.body.append(greenCharacter)
 
 let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
 document.body.append(pineTree)
+
